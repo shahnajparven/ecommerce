1e00e7029eba66a7b57744055dc35732c7d25147
shahnaj parven <shahnajritu21@gmail.com>
2023-08-06 20:19:47 +0600

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import Home from "./pages/Home";
 import "./index.css";
 import Spinner from "./components/Spinner";
 import NotFound from "./components/NotFound";
-import MainComponent from "./pages/mainComponent";
+import MainComponent from "./pages/MainComponent";
 
 function App() {
   return (
     <>
       <Suspense fallback={<Spinner />}>
         <Routes>
-        <Route path="/" element={<Home/>}/>
-          <Route path="/home" element={<MainComponent/>}>
+          <Route path="/" element={<MainComponent/>}>
             <Route index element={<Home />} />
           </Route>
           <Route path="*" element={<NotFound />} />
