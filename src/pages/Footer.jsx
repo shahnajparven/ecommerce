5af2f198dca85b626201127b27be683590e4d0b4
shahnaj parven <shahnajritu21@gmail.com>
2023-08-06 15:21:19 +0600

    copyright add

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,18 +2,31 @@ import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
-    <Box width='100vw' backgroundColor='#2D2727' display='flex' flexDirection='row' justifyContent='center' p={2}> 
-    <Typography variant="span" fontSize={12} color="white">
-        ©{' '}
-        <Link to="#1">
-          Shshnaj {currentYear}
+    <Box
+      width="100vw"
+      backgroundColor="#2D2727"
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+      p={2}
+    >
+      <Typography variant="span" fontSize={12} color="white">
+        ©{" "}
+        <Link
+          to="#"
+          style={{
+            textDecoration: "none",
+            color:'white'
+          }}
+        >
+          Basket {currentYear}
         </Link>
         . All Rights Reserved.
-    </Typography>
-</Box>
-  )
-}
+      </Typography>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
