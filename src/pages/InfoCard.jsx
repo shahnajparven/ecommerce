9eb719cc7c920f8da3b93083914ac210b6415de0
shahnaj parven <shahnajritu21@gmail.com>
2023-08-07 23:54:02 +0600

    changes

--- src/pages/InfoCard.jsx
+++ src/pages/InfoCard.jsx
@@ -3,7 +3,7 @@ import img from "../assets/info-card.jpg";
 const InfoCard = () => {
   return (
     <Box className="container" > 
-    <Box className='banner' py={5} height='50vh'>
+    <Box className='banner' py={3} pb={8} height='50vh'>
         <img src={img} height='100%' width="100%" overflow="hidden"/>
         </Box>
     </Box>
